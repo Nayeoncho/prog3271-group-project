035b12639784338ab7e92b2bc115ddd4f021a1ff
Joey <joeychanthasy@outlook.com>
2026-04-19 18:42:35 -0400

    Added post to forumRoutes

--- forum-backend/src/presentation/routes/forumRoutes.ts
+++ forum-backend/src/presentation/routes/forumRoutes.ts
@@ -2,10 +2,14 @@
 // Maps URL paths to their corresponding controller functions
 import { Router } from "express";
 import { getForumSections } from "../controllers/forumController";
+import { createdPost } from "../../application/usecases/post";
+
 
 const router = Router();
 
 // GET /api/forum-sections → runs the getForumSections controller
 router.get("/forum-sections", getForumSections);
+
+router.post("/posts", createdPost);
 
 export default router;
