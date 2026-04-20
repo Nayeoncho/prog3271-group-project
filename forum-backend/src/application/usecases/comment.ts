af061900d275da49af1257b581290b28536c55ac
Joey <joeychanthasy@outlook.com>
2026-04-19 20:20:33 -0400

    Added comment entity and respoitory interface fixed worng file location

--- forum-backend/src/application/usecases/comment.ts
+++
@@ -1,9 +0,0 @@
-export interface CommentEntity {
-    _id?: string;
-    content: string;
-    postId: string;
-    authorId: string;
-    authorName?: string;
-    createdAt?: Date;
-    updatedAt?: Date;
-}
--- forum-backend/src/domain/entities/comments.ts
+++ forum-backend/src/domain/entities/comments.ts
@@ -1,8 +1,9 @@
-import { CommentEntity } from "../../application/usecases/comment";
-
-export interface ICommentRepo {
-    create(comment: CommentEntity): Promise<CommentEntity>; // Creates a new comment in the database
-    findByPostId(postId: string): Promise<CommentEntity[]>; // Gets all comments for a specific post
-    findById(id: string): Promise<CommentEntity | null>; // Finds one comment by its ID
-    delete(id: string): Promise<CommentEntity | null>; // Deletes a comment by its ID
+export interface CommentEntity {
+    _id?: string;
+    content: string;
+    postId: string;
+    authorId: string;
+    authorName?: string;
+    createdAt?: Date;
+    updatedAt?: Date;
 }
