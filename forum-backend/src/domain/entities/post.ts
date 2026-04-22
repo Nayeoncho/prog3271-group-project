a998abbfda6170e8aeee75c1e0281c48e228e57b
Nayeon Cho <ncho2769@conestogac.on.ca>
2026-04-22 15:14:04 -0400

    add like count field in post model

--- forum-backend/src/domain/entities/post.ts
+++ forum-backend/src/domain/entities/post.ts
@@ -1,9 +1,10 @@
 export interface PostEntity {
-    _id?: string;
-    title: string;
-    content: string;
-    authorId: string;
-    authorName?: string;
-    createdAt?: Date;
-    updatedAt?: Date;
+  _id?: string;
+  title: string;
+  content: string;
+  authorId: string;
+  authorName?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+  likeCount?: number;
 }
--- forum-backend/src/infrastructure/models/Post.ts
+++ forum-backend/src/infrastructure/models/Post.ts
@@ -2,16 +2,17 @@ import mongoose from "mongoose";
 import { PostEntity } from "../../domain/entities/post";
 
 const postSchema = new mongoose.Schema<PostEntity>(
-    {
-        title: {type: String, required: true},
-        content: {type: String, required: true},
-        authorId: {type: String, required: true},
-        authorName: {type: String, required: false},
-    },
-    {
-        collection: "posts",
-        timestamps: true,
-    },
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    authorId: { type: String, required: true },
+    authorName: { type: String, required: false },
+    likeCount: { type: Number, default: 0 },
+  },
+  {
+    collection: "posts",
+    timestamps: true,
+  },
 );
 
 const PostModel = mongoose.model<PostEntity>("Post", postSchema);
